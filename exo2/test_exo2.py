b1d3400540883e01e1507124376c7e46ca1dd283
aywin <163024322+aywin@users.noreply.github.com>
2024-09-26 09:57:47 +0100

    Ajout de la solution pour l'exercice 1

---
+++ exo2/test_exo2.py
@@ -0,0 +1,34 @@
+import unittest
+from exo2 import solution
+
+class TestSolution(unittest.TestCase):
+    
+    def test_fixed_tests_True(self):
+        fixed_tests_True = [
+            ("samurai", "ai"),
+            ("ninja", "ja"),
+            ("sensei", "i"),
+            ("abc", "abc"),
+            ("abcabc", "bc"),
+            ("fails", "ails"),
+        ]
+        for string, ending in fixed_tests_True:
+            with self.subTest(string=string, ending=ending):
+                self.assertTrue(solution(string, ending))
+    
+    def test_fixed_tests_False(self):
+        fixed_tests_False = [
+            ("sumo", "omo"),
+            ("samurai", "ra"),
+            ("abc", "abcd"),
+            ("ails", "fails"),
+            ("this", "fails"),
+            ("spam", "eggs"),
+        ]
+        for string, ending in fixed_tests_False:
+            with self.subTest(string=string, ending=ending):
+                self.assertFalse(solution(string, ending))
+
+# Running the tests
+if __name__ == "__main__":
+    unittest.main()
